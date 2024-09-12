d2507860d20b40db2bbd6ab5a83084a90ddac1cc
Mahek Gondaliya <160844032+MahekGondaliya@users.noreply.github.com>
2024-09-12 22:50:36 +0530

    Update AuthController.js

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -30,6 +30,8 @@ export const login = async (req, res) => {
 
     res.cookie("jwt", createToken(email, user.id), {
       maxage,
+      secure: true,
+      sameSite: "None",
     });
 
     return res.status(200).json({
@@ -58,6 +60,8 @@ export const signup = async (req, res, next) => {
     const user = await User.create({ email, password });
     res.cookie("jwt", createToken(email, user.id), {
       maxage,
+      secure: true,
+      sameSite: "None",
     });
 
     return res.status(201).json({
@@ -180,7 +184,7 @@ export const removeProfileImg = async (req, res) => {
 export const logOut = async (req, res) => {
   try {
     
-    res.cookie("jwt" ,"" , {maxage:1})
+    res.cookie("jwt" ,"" , {maxage:1,secure:true,sameSite:"None",})
     return res.status(200).send("Logout Successfully........");
   } catch (error) {
     console.log(error.message);
